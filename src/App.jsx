8aa958bc6d2bdbac5b0046579f13a2228e709514
sanfire86626 <48641254@qq.com>
2021-11-10 11:08:26 +0800

    fix: tsconfig.json baseUrl 2

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import News from './components/News'
+const App = () => {
+    return ( 
+        <div>
+            <h3>App组件</h3>
+            <News/>
+        </div>
+     );
+}
+ 
+export default App;
