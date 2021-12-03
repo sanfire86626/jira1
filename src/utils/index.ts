74ab44c66b6e70850acc06dd02220536ec32fff4
sanfire86626 <48641254@qq.com>
2021-12-03 16:33:16 +0800

    chore: 给utils中的useDebonce加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-const isFalsy = (value: any) => (value === 0 ? false : !value);
+const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 export const cleanObject = (object: object) => {
   //进行对象深拷贝，这样不会影响传进来的对象本身
   const result = { ...object };
@@ -20,7 +20,7 @@ export const useMount = (callback: () => void) => {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 };
 
-export const useDebounce = (value: any, delay?: number) => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     const timer = setTimeout(() => setDebouncedValue(value), delay);
